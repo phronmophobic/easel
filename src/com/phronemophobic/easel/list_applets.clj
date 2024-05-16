628ab4b9dcffc985e39d8c6052537c39ec897671
Adrian Smith <smith.adriane@gmail.com>
2024-05-16 10:48:18 -0700

    update list applets.

--- src/com/phronemophobic/easel/list_applets.clj
+++ src/com/phronemophobic/easel/list_applets.clj
@@ -3,6 +3,7 @@
    [com.phronemophobic.easel.model :as model]
    [membrane.basic-components :as basic]
    [com.phronemophobic.membrandt :as ant]
+   [membrane.skia.paragraph :as para]
    [membrane.component
     :refer [defui defeffect]]
    [membrane.ui :as ui]
@@ -16,76 +17,117 @@
                :on-click on-click}))
 
 (defui list-applets-ui [{:keys [applets url $ref]}]
-  (ui/vertical-layout
-   (button {:text "Add Term"
-            ;; :hover? (get applet [::hover?])
-            :on-click
-            (fn []
-              [[:com.phronemophobic.easel/add-applet
-                (requiring-resolve 'com.phronemophobic.easel.term/termlet)]])})
+  (let [eval-ns-name (get extra ::eval-ns-name "sevengui")
+        eval-ns* (fn []
+                   (try
+                     (let [ns-sym (symbol eval-ns-name)]
+                       (require ns-sym)
+                       (the-ns ns-sym))
+                     (catch Exception e
+                       nil)))]
+    (ui/vertical-layout
+     (ui/flex-layout
+      [(para/paragraph
+        "eval-ns: "
+        nil
+        #:paragraph-style
+        {:text-style (ant/text-input-text-style {:size :small})})
+       (ant/text-input {:text eval-ns-name
+                        :size :small})]
+      {:gap 8
+       :align :center})
+     (button {:text "Add Term"
+              ;; :hover? (get applet [::hover?])
+              :on-click
+              (fn []
+                [[:com.phronemophobic.easel/add-applet
+                  (requiring-resolve 'com.phronemophobic.easel.term/termlet)]])})
 
-   (ui/horizontal-layout
-    (button {:text "Add browser"
-             ;; :hover? (get applet [::hover?])
-             :on-click
-             (fn []
-               [[:com.phronemophobic.easel/add-applet
-                 (fn [handler]
-                   ((requiring-resolve 'com.phronemophobic.easel.browser/browslet)
-                    handler
-                    url))]])})
-    (ant/text-input {:text url
-                     :size :small})
-    #_(basic/textarea {:text url}))
-   #_(button {:text "Add Schematic"
+     (ui/horizontal-layout
+      (button {:text "Add browser"
+               ;; :hover? (get applet [::hover?])
+               :on-click
+               (fn []
+                 [[:com.phronemophobic.easel/add-applet
+                   (fn [handler]
+                     ((requiring-resolve 'com.phronemophobic.easel.browser/browslet)
+                      handler
+                      url))]])})
+      (ant/text-input {:text url
+                       :size :small})
+      #_(basic/textarea {:text url}))
+     #_(button {:text "Add Schematic"
+                ;; :hover? (get applet [::hover?])
+                :on-click
+                (fn []
+                  [[:com.phronemophobic.easel/add-applet
+                    (requiring-resolve 'com.phronemophobic.easel.schematic/schematlet)]])})
+     (button {:text "Add Spreadsheet"
               ;; :hover? (get applet [::hover?])
               :on-click
               (fn []
                 [[:com.phronemophobic.easel/add-applet
-                  (requiring-resolve 'com.phronemophobic.easel.schematic/schematlet)]])})
-   (button {:text "Add Spreadsheet"
-            ;; :hover? (get applet [::hover?])
-            :on-click
-            (fn []
-              [[:com.phronemophobic.easel/add-applet
-                (requiring-resolve 'com.phronemophobic.easel.spreadsheet/spreadsheet-applet)]])})
-   #_(button {:text "Add Preview"
+                  (requiring-resolve 'com.phronemophobic.easel.spreadsheet/spreadsheet-applet)]])})
+     #_(button {:text "Add Preview"
+                ;; :hover? (get applet [::hover?])
+                :on-click
+                (fn []
+                  [[:com.phronemophobic.easel/add-applet
+                    (requiring-resolve 'com.phronemophobic.easel.ui/schema-preview)]])})
+     
+     (button {:text "Toolbar"
               ;; :hover? (get applet [::hover?])
               :on-click
               (fn []
                 [[:com.phronemophobic.easel/add-applet
-                  (requiring-resolve 'com.phronemophobic.easel.ui/schema-preview)]])})
-   (button {:text "Toolbar"
-            ;; :hover? (get applet [::hover?])
-            :on-click
-            (fn []
-              [[:com.phronemophobic.easel/add-applet
-                (requiring-resolve 'com.phronemophobic.easel.schematic2/toolbar-applet)]])})
-   (button {:text "Component Picker"
-            ;; :hover? (get applet [::hover?])
-            :on-click
-            (fn []
-              [[:com.phronemophobic.easel/add-applet
-                (requiring-resolve 'com.phronemophobic.easel.schematic2/component-picker-applet)]])})
-   (button {:text "Tree View"
-            ;; :hover? (get applet [::hover?])
-            :on-click
-            (fn []
-              [[:com.phronemophobic.easel/add-applet
-                (requiring-resolve 'com.phronemophobic.easel.schematic2/tree-applet)]])})
-   (button {:text "Preview"
-            ;; :hover? (get applet [::hover?])
-            :on-click
-            (fn []
-              [[:com.phronemophobic.easel/add-applet
-                (requiring-resolve 'com.phronemophobic.easel.schematic2/preview-applet)]])})
-   (button {:text "Detail View"
-            ;; :hover? (get applet [::hover?])
-            :on-click
-            (fn []
-              [[:com.phronemophobic.easel/add-applet
-                (requiring-resolve 'com.phronemophobic.easel.schematic2/detail-applet)]])})
-   ))
+                  (fn [handler]
+                    ((requiring-resolve 'com.phronemophobic.easel.schematic2/toolbar-applet)
+                     handler
+                     (eval-ns*)))]])})
+     (button {:text "Component Picker"
+              ;; :hover? (get applet [::hover?])
+              :on-click
+              (fn []
+                [[:com.phronemophobic.easel/add-applet
+                  (requiring-resolve 'com.phronemophobic.easel.schematic2/component-picker-applet)]])})
+     (button {:text "Tree View"
+              ;; :hover? (get applet [::hover?])
+              :on-click
+              (fn []
+                [[:com.phronemophobic.easel/add-applet
+                  (fn [handler]
+                    ((requiring-resolve 'com.phronemophobic.easel.schematic2/tree-applet)
+                     handler
+                     (eval-ns*)))]])})
+     (button {:text "Preview"
+              ;; :hover? (get applet [::hover?])
+              :on-click
+              (fn []
+                [[:com.phronemophobic.easel/add-applet
+                  (fn [handler]
+                    ((requiring-resolve 'com.phronemophobic.easel.schematic2/preview-applet)
+                     handler
+                     (eval-ns*)))]])})
+     (button {:text "Detail View"
+              ;; :hover? (get applet [::hover?])
+              :on-click
+              (fn []
+                [[:com.phronemophobic.easel/add-applet
+                  (fn [handler]
+                    ((requiring-resolve 'com.phronemophobic.easel.schematic2/detail-applet)
+                     handler
+                     (eval-ns*)))]])})
+
+     (button {:text "NS Watcher"
+              ;; :hover? (get applet [::hover?])
+              :on-click
+              (fn []
+                [[:com.phronemophobic.easel/add-applet
+                  (fn [handler]
+                    ((requiring-resolve 'com.phronemophobic.easel.ns-watcher/ns-watcher-applet)
+                     handler
+                     (eval-ns*)))]])})
+     )))
 
 
 
