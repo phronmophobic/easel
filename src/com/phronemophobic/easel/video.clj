c3a7968d508f0fa3489951f264b42538c1bf212b
Adrian Smith <smith.adriane@gmail.com>
2024-11-17 10:46:06 -0500

    Add video widget.

---
+++ src/com/phronemophobic/easel/video.clj
@@ -0,0 +1,60 @@
+(ns com.phronemophobic.easel.video
+  (:require
+   [membrane.component :refer
+    [defui
+     defeffect]]
+   [membrane.ui :as ui]
+   [clojure.java.io :as io]
+   [membrane.alpha.component.drag-and-drop :as dnd]
+   [membrane.basic-components :as basic]
+   [clojure.core.async :as async]
+   [clojure.set :as set]
+   [com.phronemophobic.clj-media.impl.skia :as video]
+   [com.phronemophobic.easel.model :as model]
+   [com.phronemophobic.membrandt :as ant]))
+
+(defn video-ui [this $context context]
+  (let [state (-> this
+                  (assoc :context context
+                         :$context $context
+                         :extra (:extra this)
+                         :$extra [(:$ref this) '(keypath :extra)]))]
+    (def video-state state)
+    (video/video-player state)))
+
+
+(defrecord VideoWidget [dispatch! path]
+  model/IApplet
+  (-start [this $ref size _content-scale]
+    (let [
+          
+          $video-state [$ref '(keypath :video-state)]
+          this (assoc this
+                      :extra {}
+                      :$ref $ref
+                      :size size
+                      :$video-state $video-state
+                      :path path)
+          ]
+
+      (dispatch! ::video/play this)
+
+      this))
+  (-stop [this]
+    (dispatch! ::video/cleanup {:video-state (:video-state this)})
+
+    nil)
+  model/IUI
+  (-ui [this $context context]
+    (video-ui this $context context))
+  model/IResizable
+  (-resize [this size _content-scale]
+    (assoc this
+           :size size)))
+
+(defn video-applet [handler ]
+  (-> (->VideoWidget handler "/var/tmp/firehose/firehose.mp4")
+      (assoc :label
+             (str "Simplicity\nFirehose"))))
+
+
