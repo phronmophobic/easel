c0b078e20f2d616a98377ac75a3a1295c1a23eb9
Adrian Smith <smith.adriane@gmail.com>
2025-08-17 10:58:04 -0700

    add drop target to tap watcher.

--- src/com/phronemophobic/easel/tap_watcher.clj
+++ src/com/phronemophobic/easel/tap_watcher.clj
@@ -13,30 +13,78 @@
 
    [com.phronemophobic.membrandt :as ant]))
 
+(defeffect ::tap-drop-object [drop-object]
+  (when-let [x (:x drop-object)]
+    (tap> @x)))
+
+
+(defui on-drag-hover
+  "Component for adding a hover? state."
+  [{:keys [object body]}]
+  (if object
+    (ui/wrap-on
+     :mouse-move-global
+     (fn [handler [x y :as pos]]
+       (let [[w h] (ui/bounds body)
+             child-intents (handler pos)]
+         (if (or (neg? x)
+                 (> x w)
+                 (neg? y)
+                 (> y h))
+           (conj child-intents
+                 [:set $object nil])
+           child-intents)))
+     body)
+    (dnd/on-drop-move
+     (fn [_ o]
+       [[:set $object o]])
+     body)))
+
+
+(defui drag-target [{:keys [drag-object body]}]
+  (dnd/on-drop
+   (fn [pos obj]
+     (let [intents [[:set $drag-object nil]]]
+       (conj intents [::tap-drop-object obj])))
+   (on-drag-hover
+    {:$body nil
+     :object drag-object
+     :body
+     (let [body [body (ui/spacer 200 200)]
+           body (if drag-object
+                  (ui/fill-bordered
+                   [1 0 0 0.2]
+                   0
+                   body)
+                  body)]
+       body)})))
 
 (defui tap-view [{:keys [tap-vals size]}]
   (basic/scrollview
    {:scroll-bounds size
     :$body nil
     :body
-    (apply
-     ui/vertical-layout
-     (ant/button {:size :small
-                  :text "X"
-                  :on-click (fn []
-                              [[:set $tap-vals []]])})
-     (eduction
-      (map-indexed
-       (fn [i obj]
-         (let [inspector-extra (get extra [::inspector [i obj]])]
-           (ui/vertical-layout
-            (viscous/inspector
-             {:obj obj
-              :width (get inspector-extra :width 40)
-              :height (get inspector-extra :height 1)
-              :show-context? (get inspector-extra :show-context?)
-              :extra inspector-extra})))))
-      tap-vals))}))
+    (drag-target
+     {:$body nil
+      :body 
+      (apply
+       ui/vertical-layout
+       (ant/button {:size :small
+                    :text "X"
+                    :on-click (fn []
+                                [[:set $tap-vals []]])})
+       (eduction
+        (map-indexed
+         (fn [i obj]
+           (let [inspector-extra (get extra [::inspector [i obj]])]
+             (ui/vertical-layout
+              (viscous/inspector
+               {:obj obj
+                :width (get inspector-extra :width 40)
+                :height (get inspector-extra :height 1)
+                :show-context? (get inspector-extra :show-context?)
+                :extra inspector-extra})))))
+        tap-vals))})}))
 
 (defn tap-ui [this $context context]
   (let [state (-> this
